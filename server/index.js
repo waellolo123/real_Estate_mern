41bdfbbd70526b67fc520f5e74b749e9efe846ff
wael <wael.22@live.fr>
2023-10-06 20:42:25 +0100

    connect to db

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ import mongoose from 'mongoose';
 const app = express();
 dotenv.config();
 
+mongoose.connect((process.env.MONGO_URL))
+.then(()=>{
+  console.log('connected to db!');
+})
+.catch((err)=>{
+  console.log(err);
+})
+
 app.listen(process.env.PORT, () => {
   console.log('server is running on port 8000');
 })
