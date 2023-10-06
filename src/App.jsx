12bb336ef73be97b0c547727c2db853f7b1575f0
wael <wael.22@live.fr>
2023-10-06 18:55:08 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
 
 
   return (
     <>
-     <h1 >hello</h1>
+    <Routes>
+     <Route path='/' element={<Home />} />
+     <Route path='/about' element={<About />} />
+     <Route path='/profile' element={<Profile />} />
+     <Route path='/signin' element={<Signin />} />
+     <Route path='/signup' element={<Signup />} />
+     
+    </Routes>
     </>
   )
 }
