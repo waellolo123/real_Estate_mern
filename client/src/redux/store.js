72a4a7ffb18fa48eb0a8600cb19d363034c33967
wael <wael.22@live.fr>
2023-10-09 22:01:59 +0100

    redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,25 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import {persistStore,persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+const persistConfig = {
+  key: 'root',
+  storage,
+  version:1,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
 })
 
 
 
-
+export const persistor = persistStore(store);
 
 
 
